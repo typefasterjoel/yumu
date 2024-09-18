947e2c2679624919288c3107aaa2ac2504beb5f5
Joel Lopez <joel.lopez@gamershive.net>
2024-09-17 22:45:38 -0700

    Adds Shadcn and Inter

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "./components/ui/button";
 
 function App() {
-  return <div className="text-red-400">App</div>;
+  return (
+    <div className="text-red-400">
+      <Button>Test</Button>
+    </div>
+  );
 }
 
 export default App;
