56d9023c5e87e4928c676c9730598589c5c9bfe3
typefasterjoel <joel@typefasterjoel.com>
2026-03-02 19:24:54 -0800

    Window Taskbar Fix
    - Windows was opening the window as a separate application instead of the same one if you add the program into the taskbar.

--- src/main/index.ts
+++ src/main/index.ts
@@ -66,7 +66,7 @@ app.commandLine.appendSwitch('disable-features', 'MediaSessionService') // Disab
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.yumu')
+  electronApp.setAppUserModelId('com.yumu.app')
 
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
